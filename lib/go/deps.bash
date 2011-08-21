548eb4ad55170f40b2052e43b4fdc57f709d9407
Aalok Shah <aalok@shah.ws>
2011-08-20 20:55:38 -0400

    Update deps.bash for support of many more directories and Make.deps for consistency

--- lib/go/deps.bash
+++ lib/go/deps.bash
@@ -3,6 +3,9 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+STARTDIR=`pwd`
+GOLIBDIR=${GOROOT}/pkg/${GOOS}_${GOARCH}
+
 OUT="Make.deps"
 TMP="Make.deps.tmp"
 
@@ -12,30 +15,78 @@ if [ -f $OUT ] && ! [ -w $OUT ]; then
 fi
 
 # Get list of directories from Makefile
-dirs=$(sed '1,/^DIRS=/d; /^$/,$d; s/\\//g' Makefile)
-dirs2=$(sed '1,/^DIRS=/d; /^$/,$d; s/\\//g' $GOROOT/src/pkg/Makefile)
-dirpat=$(echo $dirs $dirs2 | sed 's/ /|/g; s/.*/^(&)$/')
+localdirs=$(gomake --no-print-directory echo-dirs)
 
-for dir in $dirs; do (
-	cd $dir || exit 1
+dirs=""
 
-	sources=$(sed -n 's/\.go\\/.go/p' Makefile)
-	sources=$(ls $sources 2> /dev/null)  # remove .s, .c, etc.
+function addGorootDep {
+  FILENAME=$1
+  FULLPATH=${GOROOT}/pkg/${GOOS}_${GOARCH}/${FILENAME}
+  if [ -d ${FULLPATH} ]; then
+    for filename in $(ls -1 ${FULLPATH}); do
+      addGorootDep $1/$filename
+    done
+  else
+    BASEFILENAME=$(basename $1)
+    LIBNAME=$(basename -s .a $1)
+    DIRNAME=$(dirname $1)
+    if [ ${LIBNAME} != ${BASEFILENAME} ]; then
+      if [ ${DIRNAME} = "." ]; then
+        echo ${LIBNAME}.install: \${GOROOT}/pkg/\${GOOS}_\${GOARCH}/$1
+        dirs="${dirs} ${LIBNAME}"
+      else
+        echo ${DIRNAME}/${LIBNAME}.install: \${GOROOT}/pkg/\${GOOS}_\${GOARCH}/$1
+        dirs="${dirs} ${DIRNAME}/${LIBNAME}"
+      fi
+    fi
+  fi
+}
+
+pushd . > /dev/null
+(
+for filename in $(ls -1 ${GOLIBDIR}); do
+  addGorootDep $filename
+done
+
+dirpat=$(echo $dirs $localdirs C | awk '{
+	for(i=1;i<=NF;i++){ 
+		x=$i
+		gsub("/", "\\/", x)
+		printf("/^(%s)$/\n", x)
+	}
+}')
+
+for dir in $localdirs; do 
+  if [ -d $STARTDIR/$dir ]; then
+    cd $STARTDIR/$dir || exit 1
+  else
+    continue
+  fi
+
+	sources=$(sed -n 's/^[ 	]*\([^ 	]*\.go\)[ 	]*\\*[ 	]*$/\1/p' Makefile)
+	sources=$(echo $sources | sed 's/\$(GOOS)/'$GOOS'/g')
+	sources=$(echo $sources | sed 's/\$(GOARCH)/'$GOARCH'/g')
+	# /dev/null here means we get an empty dependency list if $sources is empty
+	# instead of listing every file in the directory.
+	sources=$(ls $sources /dev/null 2> /dev/null)  # remove .s, .c, etc.
+	
+	#sources=$(sed -n 's/\.go\\/.go/p' Makefile)
+	#sources=$(ls $sources 2> /dev/null)  # remove .s, .c, etc.
 
 	deps=$(
 		sed -n '/^import.*"/p; /^import[ \t]*(/,/^)/p' $sources /dev/null |
 		cut -d '"' -f2 |
-		egrep "$dirpat" |
+		awk "$dirpat" |
 		grep -v "^$dir\$" |
 		sed 's/$/.install/' |
+		sed 's;^C\.install;runtime/cgo.install;' |
 		sort -u
 	)
 
 	echo $dir.install: $deps
-) done > $TMP
+done
+) > $TMP
 
-for dir in $dirs2; do (
-  echo $dir.install: \${GOROOT}/pkg/\${GOOS}_\${GOARCH}/${dir}.a
-) done >> $TMP
+popd > /dev/null
 
 mv $TMP $OUT
