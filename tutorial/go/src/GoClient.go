d674426c69eccef65028cbb8261cfbf82fdfd652
Matt T. Proud <matt.proud@gmail.com>
2012-08-12 23:48:45 +0200

    Cleaning up cosmetic defects ``gofmt`` can fix.
    See earlier commit for rationale.

--- tutorial/go/src/GoClient.go
+++ tutorial/go/src/GoClient.go
@@ -1,6 +1,5 @@
 package main
 
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -20,73 +19,71 @@ package main
  * under the License.
  */
 
-
 import (
-  "fmt"
-  "net"
-  "os"
-  "thrift"
-  "thriftlib/tutorial"
+	"fmt"
+	"net"
+	"os"
+	"thrift"
+	"thriftlib/tutorial"
 )
 
 func Perform(client *tutorial.CalculatorClient) (err os.Error) {
-  client.Ping()
-  fmt.Print("ping()\n")
+	client.Ping()
+	fmt.Print("ping()\n")
 
-  sum, _ := client.Add(1, 1)
-  fmt.Print("1+1=", sum, "\n")
+	sum, _ := client.Add(1, 1)
+	fmt.Print("1+1=", sum, "\n")
 
-  work := tutorial.NewWork()
-  work.Op = tutorial.DIVIDE
-  work.Num1 = 1
-  work.Num2 = 0
-  quotient, ouch, err := client.Calculate(1, work)
-  if err != nil {
-    fmt.Print("Error during operation: ", err.String(), "\n")
-    return err
-  } else if ouch != nil {
-    fmt.Print("Invalid operation: ", ouch.String(), "\n")
-  } else {
-    fmt.Print("Whoa we can divide by 0 with new value: ", quotient, "\n")
-  }
+	work := tutorial.NewWork()
+	work.Op = tutorial.DIVIDE
+	work.Num1 = 1
+	work.Num2 = 0
+	quotient, ouch, err := client.Calculate(1, work)
+	if err != nil {
+		fmt.Print("Error during operation: ", err.String(), "\n")
+		return err
+	} else if ouch != nil {
+		fmt.Print("Invalid operation: ", ouch.String(), "\n")
+	} else {
+		fmt.Print("Whoa we can divide by 0 with new value: ", quotient, "\n")
+	}
 
-  work.Op = tutorial.SUBTRACT
-  work.Num1 = 15
-  work.Num2 = 10
-  diff, ouch, err := client.Calculate(1, work)
-  if err != nil {
-    fmt.Print("Error during operation: ", err.String(), "\n")
-    return err
-  } else if ouch != nil {
-    fmt.Print("Invalid operation: ", ouch.String(), "\n")
-  } else {
-    fmt.Print("15-10=", diff, "\n")
-  }
+	work.Op = tutorial.SUBTRACT
+	work.Num1 = 15
+	work.Num2 = 10
+	diff, ouch, err := client.Calculate(1, work)
+	if err != nil {
+		fmt.Print("Error during operation: ", err.String(), "\n")
+		return err
+	} else if ouch != nil {
+		fmt.Print("Invalid operation: ", ouch.String(), "\n")
+	} else {
+		fmt.Print("15-10=", diff, "\n")
+	}
 
-  log, err := client.GetStruct(1)
-  if err != nil {
-    fmt.Print("Unable to get struct: ", err.String(), "\n")
-    return err
-  } else {
-    fmt.Print("Check log: ", log.Value, "\n")
-  }
-  return err
+	log, err := client.GetStruct(1)
+	if err != nil {
+		fmt.Print("Unable to get struct: ", err.String(), "\n")
+		return err
+	} else {
+		fmt.Print("Check log: ", log.Value, "\n")
+	}
+	return err
 }
 
-
 func RunClient(transportFactory thrift.TTransportFactory, protocolFactory thrift.TProtocolFactory) os.Error {
-  addr, err := net.ResolveTCPAddr("tcp", "localhost:9090")
-  if err != nil {
-    fmt.Print("Error resolving address: ", err.String(), "\n")
-    return err
-  }
-  transport := thrift.NewTSocketAddr(addr)
-  if err = transport.Open(); err != nil {
-    fmt.Print("Error opening connection for protocol ", addr.Network(), " to ", addr.String(), ": ", err.String(), "\n")
-    return err
-  }
-  useTransport := transportFactory.GetTransport(transport)
-  client := tutorial.NewCalculatorClientFactory(useTransport, protocolFactory)
-  Perform(client)
-  return transport.Close()
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:9090")
+	if err != nil {
+		fmt.Print("Error resolving address: ", err.String(), "\n")
+		return err
+	}
+	transport := thrift.NewTSocketAddr(addr)
+	if err = transport.Open(); err != nil {
+		fmt.Print("Error opening connection for protocol ", addr.Network(), " to ", addr.String(), ": ", err.String(), "\n")
+		return err
+	}
+	useTransport := transportFactory.GetTransport(transport)
+	client := tutorial.NewCalculatorClientFactory(useTransport, protocolFactory)
+	Perform(client)
+	return transport.Close()
 }
--- tutorial/go/src/GoServer.go
+++ tutorial/go/src/GoServer.go
@@ -1,6 +1,5 @@
 package main
 
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -20,66 +19,64 @@ package main
  * under the License.
  */
 
-
 import (
-  "fmt"
-  "net"
-  "thrift"
-  "thriftlib/tutorial"
+	"fmt"
+	"net"
+	"thrift"
+	"thriftlib/tutorial"
 )
 
-
 type GoServer struct {
-  handler   tutorial.ICalculator
-  processor *tutorial.CalculatorProcessor
+	handler   tutorial.ICalculator
+	processor *tutorial.CalculatorProcessor
 }
 
 func NewGoServer() *GoServer {
-  handler := NewCalculatorHandler()
-  processor := tutorial.NewCalculatorProcessor(handler)
-  return &GoServer{handler: handler, processor: processor}
+	handler := NewCalculatorHandler()
+	processor := tutorial.NewCalculatorProcessor(handler)
+	return &GoServer{handler: handler, processor: processor}
 }
 
 func Simple(processor *tutorial.CalculatorProcessor, transportFactory thrift.TTransportFactory, protocolFactory thrift.TProtocolFactory, ch chan int) {
-  addr, err := net.ResolveTCPAddr("tcp", "localhost:9090")
-  if err != nil {
-    fmt.Print("Error resolving address: ", err.String(), "\n")
-    return
-  }
-  serverTransport, err := thrift.NewTServerSocketAddr(addr)
-  if err != nil {
-    fmt.Print("Error creating server socket: ", err.String(), "\n")
-    return
-  }
-  server := thrift.NewTSimpleServer4(processor, serverTransport, transportFactory, protocolFactory)
-  // Use this for a multithreaded server
-  // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:9090")
+	if err != nil {
+		fmt.Print("Error resolving address: ", err.String(), "\n")
+		return
+	}
+	serverTransport, err := thrift.NewTServerSocketAddr(addr)
+	if err != nil {
+		fmt.Print("Error creating server socket: ", err.String(), "\n")
+		return
+	}
+	server := thrift.NewTSimpleServer4(processor, serverTransport, transportFactory, protocolFactory)
+	// Use this for a multithreaded server
+	// TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
 
-  fmt.Print("Starting the simple server... on ", addr, "\n")
-  for {
-    err = server.Serve()
-    if err != nil {
-      fmt.Print("Error during simple server: ", err.String(), "\n")
-      return
-    }
-  }
-  fmt.Print("Done with the simple server\n")
-  ch <- 1
+	fmt.Print("Starting the simple server... on ", addr, "\n")
+	for {
+		err = server.Serve()
+		if err != nil {
+			fmt.Print("Error during simple server: ", err.String(), "\n")
+			return
+		}
+	}
+	fmt.Print("Done with the simple server\n")
+	ch <- 1
 }
 
 func Secure(processor *tutorial.CalculatorProcessor) {
-  addr, _ := net.ResolveTCPAddr("tcp", "localhost:9091")
-  serverTransport, _ := thrift.NewTNonblockingServerSocketAddr(addr)
-  server := thrift.NewTSimpleServer2(processor, serverTransport)
-  fmt.Print("Starting the secure server... on ", addr, "\n")
-  server.Serve()
-  fmt.Print("Done with the secure server\n")
+	addr, _ := net.ResolveTCPAddr("tcp", "localhost:9091")
+	serverTransport, _ := thrift.NewTNonblockingServerSocketAddr(addr)
+	server := thrift.NewTSimpleServer2(processor, serverTransport)
+	fmt.Print("Starting the secure server... on ", addr, "\n")
+	server.Serve()
+	fmt.Print("Done with the secure server\n")
 }
 
 func RunServer(transportFactory thrift.TTransportFactory, protocolFactory thrift.TProtocolFactory) {
-  server := NewGoServer()
-  ch := make(chan int)
-  go Simple(server.processor, transportFactory, protocolFactory, ch)
-  //go Secure(server.processor)
-  _ = <-ch
+	server := NewGoServer()
+	ch := make(chan int)
+	go Simple(server.processor, transportFactory, protocolFactory, ch)
+	//go Secure(server.processor)
+	_ = <-ch
 }
