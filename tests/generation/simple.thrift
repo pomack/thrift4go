c10dfddeaa406df6538cb2ae082f6150c9c7e41a
Matt T. Proud <matt.proud@gmail.com>
2012-08-16 00:11:08 +0200

    Add a test to verify the generated Go from IDL.
    We currently have no way of verifying that the generated Go code from
    the Thrift IDL is nessarily correct nor that it will even compile nor
    that the generated parts even behave correctly.  This new test case
    is an attempt to get some scaffolding around that problem domain.

---
+++ tests/generation/simple.thrift
@@ -0,0 +1,18 @@
+enum UndefinedValues {
+  Foo,
+  Bar,
+  Baz
+}
+
+enum DefinedValues {
+  Foo = 1,
+  Bar = 2,
+  Baz = 3
+}
+
+enum HeterogeneousValues {
+  Foo,
+  Bar = 2,
+  Baz,
+  Qux = 4
+}
